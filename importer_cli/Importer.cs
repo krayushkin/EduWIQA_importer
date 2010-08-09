7d0e0b7b13b923258ad71ae422d97e839a8a32b0
Krayushkin Dmitry <krayushkin.dm@gmail.com>
2010-08-09 22:55:02 +0400

    no

--- importer_cli/Importer.cs
+++ importer_cli/Importer.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Data;
 using System.Data.OleDb;
-using System.Windows.Forms;
-using System.IO;
+using importer_cli;
 
 namespace equImport
 {
@@ -134,7 +133,7 @@ namespace equImport
         }
     }
 
-    
+
 
     public static class Importer
     {
@@ -151,7 +150,7 @@ namespace equImport
         /// </returns>
         static public Dictionary<int, int> find_all_duplicates(string table, DataTable source_t, OleDbConnection target_con, OleDbCommand select_com, string primary_key)
         {
-            Dictionary<int, int> old_new_keys = new Dictionary<int,int>();
+            Dictionary<int, int> old_new_keys = new Dictionary<int, int>();
             OleDbCommand select_target = select_com.Clone();
             select_target.Connection = target_con;
             OleDbDataReader reader = select_target.ExecuteReader();
@@ -166,7 +165,7 @@ namespace equImport
                     {
                         if (col.ColumnName != primary_key)
                         {
-                            if (! source_row[col.ColumnName].Equals(target_row[col.ColumnName]))
+                            if (!source_row[col.ColumnName].Equals(target_row[col.ColumnName]))
                             {
                                 rows_equal = false;
                                 break;
@@ -187,39 +186,145 @@ namespace equImport
 
 
 
-        static public void import(TextBox log)
+        static public void import(string source_db, string target_db)
         {
-            string source_db = "X:\\ulstu\\equImport\\add\\base3\\base.mdb";
-            string target_db = "X:\\ulstu\\equImport\\base\\base.mdb";
-            using (OleDbConnection sdb_con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+source_db))
+            using (OleDbConnection sdb_con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + source_db),
+                   tdb_con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + target_db))
             {
-                using (OleDbConnection tdb_con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+target_db))
+
+                try
                 {
                     sdb_con.Open();
                     tdb_con.Open();
+                }
+                catch (OleDbException e)
+                {
+                    throw new ApplicationException(e.Message);
+                }
 
-                    Dictionary<string, Dictionary<int, int>> table_old_new_key = new Dictionary<string, Dictionary<int, int>>();
-                    Dictionary<string, TableInfo> infos = EquSchema.getSchema(sdb_con);
+                Dictionary<string, Dictionary<int, int>> table_old_new_key = new Dictionary<string, Dictionary<int, int>>();
+                Dictionary<string, TableInfo> infos = EquSchema.getSchema(sdb_con);
 
-                    List<string> already_copied = new List<string>();
-                    List<string> non_empty = Importer.non_empty_tables(sdb_con, infos);
-                    foreach (string non_empty_table in non_empty)
+                List<string> already_copied = new List<string>();
+                List<string> non_empty = Importer.non_empty_tables(sdb_con, infos);
+                foreach (string non_empty_table in non_empty)
+                {
+                    List<string> deps = new List<string>();
+                    EquSchema.traversal_table_dep_from(non_empty_table, infos, (string s) => deps.Add(s));
+                    foreach (string table in deps)
                     {
-                        List<string> deps = new List<string>();
-                        EquSchema.traversal_table_dep_from(non_empty_table, infos, (string s)=> deps.Add(s));
-                        foreach (string table in deps)
+                        if (!already_copied.Contains(table))
                         {
-                            if (!already_copied.Contains(table))
-                            {
-                                log.AppendText(String.Format("copy {0}\n", table));
-                                copy_table(table, infos, sdb_con, tdb_con, table_old_new_key);
-                                already_copied.Add(table);
-                            }
+                            Log.Writer.Write("Копирую {0}... \n", table);
+                            copy_table(table, infos, sdb_con, tdb_con, table_old_new_key);
+                            Log.Writer.Write("OK\n");
+                            already_copied.Add(table);
                         }
                     }
                 }
+
             }
-            log.AppendText("All done!\n");
+            Log.Writer.Write("All done!\n");
+        }
+
+
+        static private void copy_table_with_iowner
+           (
+           string table,
+           Dictionary<string, TableInfo> infos,
+           OleDbConnection source_con,
+           OleDbConnection target_con,
+           Dictionary<string, Dictionary<int, int>> table_old_new_key
+           )
+        {
+            OleDbCommand com = new OleDbCommand("SELECT * from " + table, source_con);
+            OleDbDataReader reader = com.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                OleDbDataAdapter adapter = new OleDbDataAdapter(com);
+                OleDbCommandBuilder builder = new OleDbCommandBuilder(adapter);
+                builder.QuotePrefix = "[";
+                builder.QuoteSuffix = "]";
+
+                DataTable data_table = new DataTable();
+                data_table.Load(reader);
+                Log.Writer.WriteLine("Найдено {0} записей.", data_table.Rows.Count.ToString());
+                table_old_new_key[table] = new Dictionary<int, int>();
+
+                int insert_counter = 0;
+                
+                Queue<DataRow> rows_queue = new Queue<DataRow>(data_table.Rows.Cast<DataRow>());
+                while (rows_queue.Count > 0)
+                {
+                    DataRow row = rows_queue.Dequeue();
+                    
+                    if (((int)row["IOwner"]) != 0)
+                    {
+                        int old_iowner = (int)row["IOwner"];
+                        if (!table_old_new_key[table].ContainsKey(old_iowner))
+                        {
+                            rows_queue.Enqueue(row);
+                            continue;
+                        }
+
+                        int new_iowner = table_old_new_key[table][old_iowner];
+                        row["IOwner"] = new_iowner;
+                        Log.Writer.WriteLine("IOwner set from {0} to {1}", old_iowner, new_iowner);
+                    }
+
+                    process_fk_ids(table, infos, table_old_new_key, row);
+
+                    int old_id = (int)row[infos[table].PrimaryKey];
+
+                    insert_row(table, target_con, builder, row);
+                    ++insert_counter;
+
+                    int new_id = get_new_id(target_con);
+                    table_old_new_key[table][old_id] = new_id;
+                    Log.Writer.WriteLine("{0}[{1}] = {2}", table, old_id, new_id);
+
+                }
+
+            }
+
+        }
+
+        private static void insert_row(string table, OleDbConnection target_con, OleDbCommandBuilder builder, DataRow row)
+        {
+            OleDbCommand target_insert = builder.GetInsertCommand();
+            target_insert.Connection = target_con;
+
+            foreach (OleDbParameter param in target_insert.Parameters)
+            {
+                param.Value = row[param.SourceColumn];
+            }
+
+            try
+            {
+                target_insert.ExecuteNonQuery();
+            }
+            catch (OleDbException)
+            {
+                show_db_error_message(table, target_insert);
+            }
+        }
+
+        private static int get_new_id(OleDbConnection target_con)
+        {
+            OleDbCommand new_id_query = new OleDbCommand("SELECT @@IDENTITY", target_con);
+            int new_id = (int)new_id_query.ExecuteScalar();
+            return new_id;
+        }
+
+        private static void process_fk_ids(string table, Dictionary<string, TableInfo> infos, Dictionary<string, Dictionary<int, int>> table_old_new_key, DataRow row)
+        {
+            foreach (FKey fkey_i in infos[table].FKeys)
+            {
+                int old_key = (int)row[fkey_i.Name];
+                row[fkey_i.Name] = table_old_new_key[fkey_i.Table][old_key];
+                Log.Writer.WriteLine("{2}: {0} -> {1}", old_key, row[fkey_i.Name], fkey_i.Name);
+            }
         }
 
         static public void copy_table
@@ -231,8 +336,23 @@ namespace equImport
             Dictionary<string, Dictionary<int, int>> table_old_new_key
             )
         {
-            OleDbCommand com = new OleDbCommand("SELECT * from "+ table, source_con);
+            List<string> tables_with_iowner = new List<string>() { "QAReg", "QAPrec", "PrecGroups"};
+            if (tables_with_iowner.Contains(table)) copy_table_with_iowner(table, infos, source_con, target_con, table_old_new_key);
+            else copy_table_wo_iowner(table, infos, source_con, target_con, table_old_new_key);
+        }
+
+        static private void copy_table_wo_iowner
+            (
+            string table,
+            Dictionary<string, TableInfo> infos,
+            OleDbConnection source_con,
+            OleDbConnection target_con,
+            Dictionary<string, Dictionary<int, int>> table_old_new_key
+            )
+        {
+            OleDbCommand com = new OleDbCommand("SELECT * from " + table, source_con);
             OleDbDataReader reader = com.ExecuteReader();
+            
             if (reader.HasRows)
             {
                 OleDbDataAdapter adapter = new OleDbDataAdapter(com);
@@ -242,48 +362,49 @@ namespace equImport
 
                 DataTable data_table = new DataTable();
                 data_table.Load(reader);
-
+                Log.Writer.WriteLine("Найдено {0} записей.", data_table.Rows.Count.ToString());
                 table_old_new_key[table] = new Dictionary<int, int>();
 
                 Dictionary<int, int> duplicates = find_all_duplicates(table, data_table, target_con, com, infos[table].PrimaryKey);
-
+                Log.Writer.WriteLine("Найдено {0} дублирующих записей", duplicates.Count.ToString());
+                int insert_counter = 0;
                 foreach (DataRow row in data_table.Rows)
                 {
-                    foreach (FKey fkey_i in infos[table].FKeys)
-                    {
-                        int old_key = (int)row[fkey_i.Name];
-                        row[fkey_i.Name] = table_old_new_key[fkey_i.Table][old_key];
-                    }
-
-                    OleDbCommand target_insert = builder.GetInsertCommand();
-                    target_insert.Connection = target_con;
-
-                    foreach (OleDbParameter param in target_insert.Parameters)
-                    {
-                        param.Value = row[param.SourceColumn];
-                    }
-
+                    process_fk_ids(table, infos, table_old_new_key, row);
                     int old_id = (int)row[infos[table].PrimaryKey];
+
                     // пробуем найти дубликаты
-                    if ( duplicates.ContainsKey( old_id ))
+                    if (duplicates.ContainsKey(old_id))
                     {
                         int new_id = duplicates[old_id];
                         table_old_new_key[table][old_id] = new_id;
                     }
                     else
                     {
-                        target_insert.ExecuteNonQuery();
-                        OleDbCommand new_id_query = new OleDbCommand("SELECT @@IDENTITY", target_con);
-                        int new_id = (int)new_id_query.ExecuteScalar();
-                        table_old_new_key[table][old_id] = new_id;
-                    }
+                        insert_row(table, target_con, builder, row);
+                        ++insert_counter;
 
-                    
+                        int new_id = get_new_id(target_con);
+                        table_old_new_key[table][old_id] = new_id;
+                        Log.Writer.WriteLine("{0}[{1}] = {2}", table, old_id, new_id);
+                    }
                 }
-                
+                Log.Writer.WriteLine("Вставлено {0} записей.", insert_counter.ToString());
 
             }
+            else Log.Writer.WriteLine("Записей не найдено");
 
+        }
+
+        private static void show_db_error_message(string table, OleDbCommand target_insert)
+        {
+            string param = "";
+            foreach (OleDbParameter p in target_insert.Parameters)
+            {
+                param += String.Format("{0} ({1}): {2}\n", p.SourceColumn, p.OleDbType.ToString(), p.Value.ToString());
+            }
+            Log.Writer.Write("Ошибка при добавлении записи со следующим содержанием:\n\n{0}\nв таблицу {1}.\n", param, table);
+            throw new ApplicationException("Ошибка при добавлении записи.");
         }
 
         static public List<string> non_empty_tables(OleDbConnection connection, Dictionary<string, TableInfo> infos)
---
+++ importer_cli/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace importer_cli
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string help_str =
+@"Импорт курсов в из одной базы данных в другую.
+Использование:
+ИМЯ_ПРОГРАММЫ --source_db=SOURCE_PATH --target_db=TARGET_PATH || [--help]
+  --source_db=SOURCE_PATH  путь SOURCE_PATH к базе данных источника курсов
+  --target_db=TARGET_PATH  путь TARGET_PATH к целевой базе данных в которую
+                           будут скопированы курсы
+  --help                   вывод этой справки
+";
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine(help_str);
+                return 1;
+            }
+
+            Dictionary<string, string> param = new Dictionary<string, string>();
+            foreach (string com in args)
+            {
+                char[] sep = new char[] { '=' };
+                string[] command_value = com.Split(sep);
+                if (command_value.Length == 1)
+                    param[command_value[0]] = "";
+                else
+                    param[command_value[0]] = command_value[1];
+            }
+
+            if (param.ContainsKey("--help")) System.Console.WriteLine(help_str);
+            else
+            {
+
+                string s_db_path;
+                string t_db_path;
+                try
+                {
+                    s_db_path = param["--source_db"];
+                    t_db_path = param["--target_db"];
+                }
+                catch (System.Collections.Generic.KeyNotFoundException)
+                {
+                    Console.WriteLine("Не найдены необходимые параметры для работы! Читайте --help.");
+                    return 1;
+                }
+
+                try
+                {
+                    // Делаем всю основную работу здесь
+                    Log.InitLog();
+                    equImport.Importer.import(s_db_path, t_db_path);
+
+                }
+                catch (ApplicationException e)
+                {
+                    Console.WriteLine(String.Format("При работе программы возникла ошибка: {0}\n Выходим.", e.Message));
+                }
+                finally
+                {
+                    Log.EndLog();
+                }
+            }
+            return 0;
+        }
+    }
+
+    static public class Log
+    {
+        static public void InitLog()
+        {
+            Writer = new System.IO.StreamWriter("log.log");
+            Writer.WriteLine("----------log started------------");
+        }
+
+        static public void EndLog()
+        {
+            Writer.WriteLine("-----------log ended-------------");
+            Writer.Dispose();
+        }
+
+        static public System.IO.TextWriter Writer { set; get; }
+    }
+}
